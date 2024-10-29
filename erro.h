a0068963c29bb1892ce55a5fbecbd4160dcc49dd
Italo Carlos <italo.carlos@usp.br>
2024-10-29 12:00:51 -0300

    bunitin

--- erro.h
+++ erro.h
@@ -3,7 +3,6 @@
 #ifndef ERRO_H
 #define ERRO_H
 
-
 typedef enum{
     SUCESSO, // Operação executada com sucesso (0)
     ERRO_ALLOC, // Erro de alocação de memória (1)
@@ -12,7 +11,5 @@ typedef enum{
     ERRO_POP_VAZIO, // Tentativa de retirar elemento em uma estrutura vazia (4)
     ERRO_DEALLOC // Erro ao desalocar memória (5)
 }tipo_erro;
-
-
 
 #endif // ERRO_H
